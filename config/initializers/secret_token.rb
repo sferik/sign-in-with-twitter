57d68b90286416312d9037b0a567448b9d51adff
Erik Michaels-Ober <sferik@gmail.com>
2011-05-06 15:33:03 -0700

    Create fresh Rails 3.1.0.beta1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignInWithTwitter::Application.config.secret_token = 'ff2d6c0d9edd8f7cedc2d2df25710f08ab212971fd3bcdefbfa73ab1b002f4096a7be6fdba5445d83f59212d093faa053c61ecefc5e26749bbb292e7466d705f'
