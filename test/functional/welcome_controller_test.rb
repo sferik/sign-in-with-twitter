8a72414c6e4f9e59432b58fd893c059401c992fa
Erik Michaels-Ober <sferik@gmail.com>
2011-05-07 08:46:10 -0700

    Add more tests for home page

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -4,5 +4,14 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Sign in with Twitter"
+    assert_select "form" do
+      assert_select "[action=?]", "/auth/twitter"
+      assert_select "[method=?]", "get"
+    end
+    assert_select "input" do
+      assert_select "[type=?]", "image"
+      assert_select "[value=?]", "Sign in"
+    end
   end
 end
