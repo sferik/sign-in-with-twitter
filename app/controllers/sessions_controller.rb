6f4dbc2738cd8b8de3063e6e5fd88b817e9306d3
Erik Michaels-Ober <sferik@gmail.com>
2011-05-07 09:09:41 -0700

    Handle OAuth failures

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < ApplicationController
   def create
     redirect_to root_path, notice: "Signed in to Twitter!"
   end
+
+  def error
+    redirect_to root_path, error: "Sign in failed!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SignInWithTwitter::Application.routes.draw do
   root :to => 'welcome#index'
-  get '/auth/twitter/callback', :to => 'sessions#create', :as => :callback
+  get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
+  get '/auth/failure', to: 'sessions#error', as: 'failure'
 end
