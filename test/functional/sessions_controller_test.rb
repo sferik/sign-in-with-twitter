5c9620fdd265450e4d3ff83cd2ca7a20acdb4a55
Erik Michaels-Ober <sferik@gmail.com>
2011-05-07 11:25:18 -0700

    Add test for profile page

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,13 +1,45 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should create session on OAuth callback" do
+  test 'should set session on OAuth callback' do
     result = {}
     result['omniauth.auth'] = {}
-    result['omniauth.auth']['user_info'] = {"nickname"=>"sferik", "name"=>"Erik Michaels-Ober", "location"=>"San Francisco", "image"=>"http://a0.twimg.com/profile_images/1279736243/Github_Square_normal.jpg", "description"=>"A mind forever voyaging through strange seas of thought, alone.", "urls"=>{"Website"=>"https://github.com/sferik", "Twitter"=>"http://twitter.com/sferik"}}
-    request.stubs(:env => result)
-    get :create, oauth_token: "abc", oauth_verifier: "123"
+    result['omniauth.auth']['user_info'] = user
+    request.stubs(env: result)
+    get :create, oauth_token: 'abc', oauth_verifier: '123'
     assert_not_nil session['user_info']
+    assert_equal 'Signed in with Twitter!', flash[:notice]
     assert_redirected_to show_path
+  end
+
+  test 'should display profile if session is set' do
+    session['user_info'] = user
+    get :show
+    assert_not_nil assigns(:user)
+    assert_response :success
+    assert_select 'title', 'Sign in with Twitter'
+    assert_select 'dt', count: user.size
+    assert_select 'dd', count: user.size
+    assert_select 'form' do
+      assert_select '[action=?]', '/signout'
+      assert_select '[method=?]', 'post'
+    end
+    assert_select 'input[name="_method"]' do
+      assert_select '[type=?]', 'hidden'
+      assert_select '[value=?]', 'delete'
+    end
+    assert_select 'input[name="commit"]' do
+      assert_select '[type=?]', 'submit'
+      assert_select '[value=?]', 'Sign out'
+    end
+  end
+
+  test 'should redirect to failure path if session is blank' do
+    get :show
+    assert_redirected_to failure_path
+  end
+
+  def user
+    {'nickname' => 'sferik', 'name' => 'Erik Michaels-Ober', 'location' => 'San Francisco', 'image' => 'http://a0.twimg.com/profile_images/1279736243/Github_Square_normal.jpg', 'description' => 'A mind forever voyaging through strange seas of thought, alone.', 'urls' => {'Website' => 'https://github.com/sferik', 'Twitter' => 'http://twitter.com/sferik'}}
   end
 end
--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
-    assert_select "title", "Sign in with Twitter"
-    assert_select "form" do
-      assert_select "[action=?]", "/auth/twitter"
-      assert_select "[method=?]", "get"
+    assert_select 'title', 'Sign in with Twitter'
+    assert_select 'form' do
+      assert_select '[action=?]', '/auth/twitter'
+      assert_select '[method=?]', 'get'
     end
-    assert_select "input" do
-      assert_select "[type=?]", "image"
-      assert_select "[value=?]", "Sign in"
+    assert_select 'input[name="commit"]' do
+      assert_select '[type=?]', 'image'
+      assert_select '[value=?]', 'Sign in'
     end
   end
 end
