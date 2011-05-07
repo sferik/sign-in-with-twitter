5b3f31d3a74ad2ddf722ade1be06b565a266b956
Erik Michaels-Ober <sferik@gmail.com>
2011-05-07 10:37:34 -0700

    Create profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    redirect_to root_path, notice: "Signed in to Twitter!"
+    session['user_info'] = request.env['omniauth.auth']['user_info']
+    redirect_to show_path, notice: "Signed in with Twitter!"
+  end
+
+  def show
+    if session['user_info'].blank?
+      redirect_to failure_path
+    else
+      @user = session['user_info']
+    end
   end
 
   def error
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ SignInWithTwitter::Application.routes.draw do
   root :to => 'welcome#index'
   get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
   get '/auth/failure', to: 'sessions#error', as: 'failure'
+  get '/profile', to: 'sessions#show', as: 'show'
 end
