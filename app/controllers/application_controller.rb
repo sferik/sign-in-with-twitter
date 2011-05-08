3e66274447997a725ac364e6cab704e782b49750
Erik Michaels-Ober <sferik@gmail.com>
2011-05-07 20:08:17 -0700

    Move client method into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def client
+    Twitter.configure do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.oauth_token = session['access_token']
+      config.oauth_token_secret = session['access_secret']
+    end
+    @client ||= Twitter::Client.new
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,16 +22,4 @@ class SessionsController < ApplicationController
     reset_session
     redirect_to root_path, :notice => "Signed out!"
   end
-
-  private
-
-  def client
-    Twitter.configure do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.oauth_token = session['access_token']
-      config.oauth_token_secret = session['access_secret']
-    end
-    @client ||= Twitter::Client.new
-  end
 end
