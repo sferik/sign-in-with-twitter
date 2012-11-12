37446885ed05bf5cfc8946933b757bc8e9cd1379
Erik Michaels-Ober <sferik@gmail.com>
2012-11-12 02:48:27 -0800

    Update mocha dependency to version 0.13.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,5 @@ require 'simplecov'
 SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'mocha'
+require 'mocha/setup'
 require 'webmock/test_unit'
