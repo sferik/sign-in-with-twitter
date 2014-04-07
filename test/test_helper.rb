fec76d6286ab2d3e0a91615492bf6da9b8c90151
Erik Michaels-Ober <sferik@gmail.com>
2014-04-07 10:15:35 +0200

    Update rubocop to version 0.20.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,10 @@ ENV['RAILS_ENV'] = 'test'
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start('rails')
 
 require File.expand_path('../../config/environment', __FILE__)
