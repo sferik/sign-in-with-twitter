8d5ca1797f1a81fc00fded77fe4fe1b8f8da3fcf
Erik Michaels-Ober <sferik@gmail.com>
2014-05-14 15:09:39 +0200

    Update twitter to version 5.9.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ private
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['CONSUMER_KEY']
       config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.oauth_token = session['access_token']
-      config.oauth_token_secret = session['access_token_secret']
+      config.access_token = session['access_token']
+      config.access_token_secret = session['access_token_secret']
     end
   end
 end
