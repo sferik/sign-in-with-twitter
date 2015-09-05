6784e96cd967434aabe432a92cd53fe1856b5418
Erik Michaels-Ober <sferik@gmail.com>
2015-09-05 10:08:47 +0200

    Update rubocop to version 0.34.0

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,15 +2,13 @@ worker_processes 4
 timeout 30
 preload_app true
 
-before_fork do |server, worker|
+before_fork do
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 end
 
-after_fork do |server, worker|
+after_fork do
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
 end
