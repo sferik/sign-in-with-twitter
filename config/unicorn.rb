ddf50ad8052edb70574e0ef7a6e3f39f23270707
Erik Michaels-Ober <sferik@gmail.com>
2015-09-10 21:00:40 +0200

    Use 3 unicorn worker processes instead of 4

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
-worker_processes 4
+worker_processes 3
 timeout 30
 preload_app true
 
-before_fork do
+before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     Process.kill 'QUIT', Process.pid
   end
 end
 
-after_fork do
+after_fork do |_server, _worker|
   Signal.trap 'TERM' do
   end
 end
